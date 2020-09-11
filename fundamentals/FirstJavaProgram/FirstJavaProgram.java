fb19879a95a0df8f86fa9768ef033370040bb88f
Garrett Bass <garrettbass@MacBook-Pro.attlocal.net>
2020-09-11 15:14:10 -0500

    FirstJavaProgram commit

---
+++ fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("Myy Name is Garrett Bass");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Carrollton, TX");
+    }
+}

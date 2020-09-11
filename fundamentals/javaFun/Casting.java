6f8751e9399fad4dc912d6887f4e54f8378a7849
Garrett Bass <garrettbass@MacBook-Pro.attlocal.net>
2020-09-11 15:58:23 -0500

    more JavaFun

---
+++ fundamentals/javaFun/Casting.java
@@ -0,0 +1,14 @@
+class Casting {
+    public static void main(String[] args){
+        double d = 35.25;
+        double dd = 35.99;
+
+        //vastig the double d into an int
+        int i = (int) d; 
+
+        //castimg the double dd into an int 
+        int ii = (int) dd;
+        System.out.println(i);
+        System.out.println(ii);
+    }
+}
---
+++ fundamentals/javaFun/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+    public static void main(String[] args){
+        int ourInt; //declaring a variable without assigning a value 
+        ourInt = 400; // assigning the value to a variable (even if it is later in our code)
+        double pi = 3.14159265; //we can also declare on the same line (just like other languages)
+        boolean bool = true;
+        char singleCharacter = "A"; 
+        String multipleCharacters = "ABC";
+    }
+}
